355e4668c33f7b5a09aacd29b67a8b6f87c1358d
agarwalanamya <35624725+agarwalanamya@users.noreply.github.com>
2019-11-30 14:00:54 +0530

    Create Add One To Number.cpp

---
+++ Arrays/ArrayMath/Add One To Number.cpp
@@ -0,0 +1,48 @@
+/*
+
+Given a non-negative number represented as an array of digits,
+
+add 1 to the number ( increment the number represented by the digits ).
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+
+If the vector has [1, 2, 3]
+
+the returned vector should be [1, 2, 4]
+
+as 123 + 1 = 124.
+
+ NOTE: Certain things are intentionally left unclear in this question which you should practice asking the interviewer.
+For example, for this problem, following are some good questions to ask :
+Q : Can the input have 0’s before the most significant digit. Or in other words, is 0 1 2 3 a valid input?
+A : For the purpose of this question, YES
+Q : Can the output have 0’s before the most significant digit? Or in other words, is 0 1 2 4 a valid output?
+A : For the purpose of this question, NO. Even if the input has zeroes before the most significant digit.
+
+*/
+
+vector<int> Solution::plusOne(vector<int> &A) {
+    
+    int n = A.size();
+    int carry = 0;
+    
+    int tmp = A[n-1];
+    A[n-1] = (A[n-1] + 1) % 10;
+    carry = (tmp + 1) / 10;
+    for(int i = n-2; i >= 0; i--){
+        tmp = A[i];
+        A[i] = (carry + A[i]) % 10;
+        carry = (carry + tmp) / 10;
+    }
+    if(carry != 0){
+        A.insert(A.begin(), carry);
+    }
+    
+    while(A[0] == 0){
+        A.erase(A.begin());
+    }
+    return A;
+    
+}
