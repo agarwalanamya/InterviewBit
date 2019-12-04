90c1d9a4864864eae5c6fa2109ba0e3405b7d88c
agarwalanamya <35624725+agarwalanamya@users.noreply.github.com>
2019-12-04 14:44:17 +0530

    Update Number of 1 Bits.cpp

--- Bit_Manipulation/Number of 1 Bits.cpp
+++ Bit_Manipulation/Number of 1 Bits.cpp
@@ -9,20 +9,34 @@ The 32-bit integer 11 has binary representation
 so the function should return 3.
 */
 /*
+Solution - The idea is to the iterations taken to unset the set bits.
+The solution given traverses through every bit and then checks if the right most bit is set or not and then shifts the binary number 
+by one to the right.
+
+Another approach to reduce the number of iterations can be done by using Brian Kernighan's Algo. 
+There we transform the number by using bitwise &, by doing A = A & (A-1). This reduces the number of itearations and number of set bits is 
+the number of times loop runs.
+*/
 int Solution::numSetBits(unsigned int A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
 
-    int c = 0;
-    
+    int count = 0;
+
     while(A != 0){
         if( A & 1 ){
-            ++c;
+            ++count;
         }
         A >>= 1;
     }
-    return c;
-    
+    // This can be  done to implement Brian Kernighan's Algo.
+    /*
+    while( A != 0){
+        A &= A-1;
+        ++c;
+    }
+    */  
+    return count;
 }
